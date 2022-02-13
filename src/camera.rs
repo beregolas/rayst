5b10e21a9f145f85027e2f57741d0dc06d76be8b
wolfi <w0lf1@posteo.org>
2022-02-13 21:28:12 +0100

    rays done, started work on camera

--- src/camera.rs
+++ src/camera.rs
@@ -9,7 +9,7 @@ pub trait Camera {
 
 
 fn prepare_input(coords: Vec2) -> Vec2 {
-    (coords - 0.5) * 2
+    (coords - 0.5) * 2.
 }
 
 
@@ -27,8 +27,8 @@ impl OrthographicCamera {
     pub fn new(origin: Vec3, forward: Vec3, up: Vec3, size: Vec2) -> Self {
         let f = forward.normalize();
         let u = up.normalize();
-        debug_assert!(!forward.is_nan());
-        debug_assert!(!up.is_nan());
+        debug_assert!(!f.is_nan());
+        debug_assert!(!u.is_nan());
         let r = f.cross(u).normalize();
         Self {
             origin,
@@ -41,7 +41,6 @@ impl OrthographicCamera {
 
 impl Camera for OrthographicCamera {
     fn at(&self, coords: Vec2) -> Ray {
-        // take input from 0 to 1 and normalize it to -1 to 1
         let c = prepare_input(coords);
         Ray{
             origin: self.up * -c.y + self.right * c.x,
@@ -52,8 +51,8 @@ impl Camera for OrthographicCamera {
 
 #[derive(Debug)]
 pub struct PerspectiveCamera {
-    // Forward is scaled to the distance to the camera plane
-    // up and right are scaled to half width & height
+    // Forward is normalized
+    // up and right are scaled to half width & height of image plane at distance 1
     origin: Vec3,
     forward: Vec3,
     up: Vec3,
@@ -61,16 +60,73 @@ pub struct PerspectiveCamera {
 }
 
 impl PerspectiveCamera {
-    pub fn new(origin: Vec3, forward: Vec3, up: Vec3, size: Vec2, fov: f32) -> Self {
+    pub fn new(origin: Vec3, forward: Vec3, up: Vec3, aspect: f32, fov: f32) -> Self {
+        let f = forward.normalize();
+        let u = up.normalize();
+        debug_assert!(!f.is_nan());
+        debug_assert!(!u.is_nan());
+        let r = f.cross(u).normalize();
+        // opening angle between forward and view border
+        let alpha = fov / 180. * std::f32::consts::PI / 2.;
+        let size = Vec2::new(alpha.tan(), alpha.tan() / aspect);
         return Self {
             origin,
-
+            forward: f,
+            right: r * size.x,
+            up: u * size.y
         }
     }
 }
 
 impl Camera for PerspectiveCamera {
     fn at(&self, coords: Vec2) -> Ray {
-        todo!()
+        let c = prepare_input(coords);
+        Ray::new(self.origin, self.forward + self.up * -c.y + self.right * c.x)
     }
+}
+
+#[cfg(test)]
+mod camera_tests {
+
+    #[cfg(test)]
+    mod perspective_tests {
+        use bevy_math::{Vec2, Vec3};
+        use crate::camera::{Camera, PerspectiveCamera};
+
+        #[test]
+        fn create_success() {
+            let c1 = PerspectiveCamera::new(
+                Vec3::new(10., 10., 0.),
+                Vec3::new(-1., -1., 0.),
+                Vec3::new(1., 0., 0.),
+                4. / 3.,
+                90.
+            );
+            assert_eq!(c1.origin, Vec3::new(10., 10., 0.));
+            assert_eq!(c1.forward, Vec3::new(-1., -1., 0.).normalize());
+        }
+
+        #[test]
+        fn project() {
+            let c1 = PerspectiveCamera::new(
+                Vec3::new(10., 10., 0.),
+                Vec3::new(-1., -1., 0.),
+                Vec3::new(1., 0., 0.),
+                4. / 3.,
+                90.
+            );
+            assert!(
+                (c1.at(Vec2::new(0.5, 0.5)).at(Vec3::new(10., 10., 0.).length()) - Vec3::new(0., 0., 0.)).length() < 0.00005
+            )
+        }
+    }
+
+    #[cfg(test)]
+    mod orthogonal_tests {
+        #[test]
+        fn create_success() {
+
+        }
+    }
+
 }
