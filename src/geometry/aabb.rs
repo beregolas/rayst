9400f7d5293ed696ff7910720612e79c4d6adb92
julius <juliuskilger@posteo.de>
2022-11-27 15:06:05 +0100

    change to PC

--- src/geometry/aabb.rs
+++ src/geometry/aabb.rs
@@ -12,41 +12,52 @@ impl Geometry for Aabb {
     fn intersect(&self, ray: Ray) -> Option<Hit> {
         // intersection test with hit points
         // holds farthest intersect with closest planes of each axis
-        let mut t_min = f32::NEG_INFINITY;
+        let mut t_min: f32 = f32::NEG_INFINITY;
         // holds closest intersect with farthest planes of each axis
-        let mut t_max = f32::INFINITY;
+        let mut t_max: f32 = f32::INFINITY;
 
-        let axes = [Vec3::X, Vec3::Y, VEC3::Z, -Vec3::X, -Vec3::Y, -Vec3::Z];
+        let mut norm: usize = 0;
+        let mut sign: i32 = 1;
 
         // check all 2 planes in all 3 axis
         for i in 0..3 {
             // compute hit-point on axis plane for min and max in dimension i
-            let t1 = (self.min[i] - ray.origin[i]) * ray.recip_direction[i];
-            let t2 = (self.max[i] - ray.origin[i]) * ray.recip_direction[i];
+            let t1: f32 = (self.min[i] - ray.origin[i]) * ray.recip_direction[i];
+            let t2: f32 = (self.max[i] - ray.origin[i]) * ray.recip_direction[i];
 
             // update the farthest closest and closest farthest hit-points
             if t1 < t2 {
-                t_max = t_max.min(t2);
-                if t_min <
+                if t2 < t_max {
+                    t_max = t2;
+                }
+                if t1 > t_min {
+                    t_min = t1;
+                    norm = i;
+                    sign = 1;
+                }
             } else {
-                t_max = t_max.min(t1);
-
+                if t1 < t_max {
+                    t_max = t1;
+                }
+                if t2 > t_min {
+                    t_min = t2;
+                    norm = i;
+                    sign = -1;
+                }
             }
-
-            t_min = t_min.max(t1.min(t2));
-
         };
-        // if t_max is farther away than t_min and the hit-poit is > 0 (in front of the ray), we hit
+        // if t_max is farther away than t_min and the hit-point is > 0 (in front of the ray), we hit
         if !(t_max > t_min.max(0.)) {
             return None;
         };
         // we have a hit at t_min, compute hit point
-        let hit_point = ray.at(t_min);
-        None
+        let hit_point: Vec3 = ray.at(t_min);
+        let normal: Vec3 = Vec3::AXES[norm];
+        Some(Hit::new(hit_point, normal, t_min))
     }
 
     fn does_intersect(&self, ray: Ray) -> bool {
-        // branchless, fast intersection test
+        // branch-less, fast intersection test
         // holds farthest intersect with closest planes of each axis
         let mut t_min = f32::NEG_INFINITY;
         // holds closest intersect with farthest planes of each axis
@@ -61,11 +72,16 @@ impl Geometry for Aabb {
             t_min = t_min.max(t1.min(t2));
             t_max = t_max.min(t1.max(t2));
         };
-        // if t_max is farther away than t_min and the hit-poit is > 0 (in front of the ray), we hit
+        // if t_max is farther away than t_min and the hit-point is > 0 (in front of the ray), we hit
         t_max > t_min.max(0.)
     }
 
     fn bounds(&self) -> Aabb {
         self.clone()
     }
+}
+
+#[cfg(test)]
+mod aabb_tests {
+    
 }
