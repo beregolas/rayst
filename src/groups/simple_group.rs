e65487100386bfd854d7fb1c3e74a0dcd10beb2e
julius <juliuskilger@posteo.de>
2022-12-20 16:10:55 +0100

    switching to PC

--- src/groups/simple_group.rs
+++ src/groups/simple_group.rs
@@ -2,16 +2,28 @@ use crate::geometry::{Aabb, Geometry, Hit};
 use crate::ray::Ray;
 
 pub struct SimpleGroup {
+    list: Vec<Box<dyn Geometry>>
+}
 
+impl SimpleGroup {
+    pub fn new() -> Self {
+        Self {
+            list: Vec::new()
+        }
+    }
+
+    pub fn push(&mut self, item: Box<dyn Geometry>) {
+        self.list.push(item)
+    }
 }
 
 impl Geometry for SimpleGroup {
     fn intersect(&self, ray: &Ray) -> Option<Hit> {
-        todo!()
+        self.list.intersect(ray)
     }
 
     fn does_intersect(&self, ray: &Ray) -> bool {
-        todo!()
+        self.list.does_intersect(ray)
     }
 
     fn get_bounds(&self) -> Aabb {
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
 use crate::camera::{Camera, OrthographicCamera, PerspectiveCamera};
 use crate::color::Color;
 use crate::geometry::{Aabb, Sphere, Triangle};
+use crate::groups::simple_group::SimpleGroup;
 use crate::math::{Vec2, Vec3};
 
 pub mod geometry;
@@ -24,7 +25,9 @@ fn main() {
     // let cam = OrthographicCamera::new(Vec3::new(-10., 0., 0.), Vec3::new(1., 0., 0.), Vec3::new(0., 1., 0.), Vec2::new(2., 2.));
     // let world = Sphere::new(Vec3::new(0., 0., 0.), 1.);
     // let world = Aabb::new(Vec3::new(0., 0., 0.), Vec3::new(1., 1., 1.));
-    let world = Triangle::new(Vec3::new(0., 0., 0.), Vec3::new(0., 2., 0.), Vec3::new(2., 0., 0.));
+    // let world = Triangle::new(Vec3::new(0., 0., 0.), Vec3::new(0., 2., 0.), Vec3::new(2., 0., 0.));
+    let abox = Aabb::new(Vec3::new(0., 0., 0.), Vec3::new(1., 1., 1.));
+    let world = SimpleGroup::new(Box::new(abox));
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let ray = cam.at(Vec2::new(x as f32 / resolution.0 as f32, y as f32 / resolution.1 as f32));
