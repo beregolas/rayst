b400ada480c5f7d752e1d96c9c56c8c043860ad2
julius <juliuskilger@posteo.de>
2022-12-24 14:17:44 +0100

    lambertian initial build

---
+++ src/materials/lambertian.rs
@@ -0,0 +1,13 @@
+use crate::color::Color;
+use crate::materials::Material;
+use crate::math::{Vec3, Vector};
+
+pub struct Lambertian{
+    color: Color
+}
+
+impl Material for Lambertian {
+    fn brdf(&self, color_in: Color, light_in: Vec3, light_out: Vec3, normal: Vec3) -> Color {
+        color_in * light_in.dot(&normal)
+    }
+}
--- src/materials/mod.rs
+++ src/materials/mod.rs
@@ -1,5 +1,8 @@
+use crate::color::Color;
+use crate::math::Vec3;
+
 pub mod lambertian;
 
-pub trait BRDF {
-
+pub trait Material {
+    fn brdf(&self, color_in: Color, light_in: Vec3, light_out: Vec3, normal: Vec3) -> Color;
 }
